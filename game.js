048a8a3ecc367e8cd8143c78c5fa24cdbe687d70
Robert Kawecki <robert+github@rkaw.pl>
2018-01-07 12:40:10 +0100

    Add a clickable board cell

--- game.js
+++ game.js
@@ -1,11 +1,35 @@
-const gameContainer = document.querySelector('#game');
-const cell = document.createElement('div');
-const textNode = document.createTextNode('Hell, world!')
-cell.appendChild(textNode);
-gameContainer.appendChild(cell);
+'use strict';
 
-let onClick = function onClick() {
-  cell.textContent = 'Clicked';
-};
+class Component {
+  getElement() {
+    return this._element;
+  }
+}
 
-cell.addEventListener('click', onClick);
+class CellComponent extends Component {
+  constructor() {
+    super();
+    this._state = 'unknown';
+    this._element = document.createElement('td');
+    this._element.addEventListener('click', function() {
+      this.setState('miss');
+    }.bind(this));
+    this._refresh();
+  }
+
+  setState(stateName) {
+    this._state = stateName;
+    this._refresh();
+  }
+
+  _refresh() {
+    this._element.textContent = this._state;
+    this._element.className = 'cell_' + this._state;
+  }
+}
+
+
+const myCell = new CellComponent();
+document
+  .getElementById('cellContainer')
+  .appendChild(myCell.getElement());
